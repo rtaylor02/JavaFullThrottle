3eed976ec053b5490ac4a0c82121c7e502f3cf42
rtaylor02 <taylor.rod.j@gmail.com>
2022-07-14 06:46:01 +0100

    added intro line

--- JavaFullThrottle_IntelliJ_NetBeans_Projects/MyClass.java
+++ JavaFullThrottle_IntelliJ_NetBeans_Projects/MyClass.java
@@ -3,5 +3,6 @@ package rodtheprogrammer;
 public class MyClass {
   public static void main(String...args) {
     System.out.println("I am the best");
+    System.out.println("This is Rod here");
   }
 }
